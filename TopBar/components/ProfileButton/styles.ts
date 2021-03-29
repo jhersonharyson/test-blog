f63c32a2b53eb5bb64c391dc707d476264cdcbfa
jhersonharyson <jherson_k-f@hotmail.com>
2021-03-29 18:58:03 -0300

    2 files added

---
+++ TopBar/components/ProfileButton/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 80px;
+  padding: 8px;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  align-itens: center !important;
+  justify-content: center;
+  background-color: #fff;
+
+  &:hover {
+    cusor: pointer;
+    background-color: #f2f2f2;
+  }
+
+  & > * {
+    color: #565151;
+  }
+
+  & svg > * {
+    color: #70b5f9;
+  }
+
+  svg {
+    margin-left: 19px;
+  }
+`;
